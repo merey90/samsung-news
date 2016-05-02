f58a4c1d2bb0ef55876b26b2deb3f54bb41632bf
merey90 <merey.90@gmail.com>
2016-05-02 06:58:03 +0000

    fixed categories

--- SamsungNews/www/js/controllers.js
+++ SamsungNews/www/js/controllers.js
@@ -5,24 +5,19 @@ angular.module('starter.controllers', [])
 })
 
 .controller('NewslettersCtrl', function($scope, Newsletters) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  Newsletters.all().async().then(function(d) {
+  
+  var newsParams = {'paged':'1', 'orderby': 'date', 'action': 'post_lists', 'posttype': 'post', 'postperpage': '30', 'from': 'main'};
+  Newsletters.all().async(newsParams).then(function(d) {
     $scope.newsletters = d;
   });
-  
-  // $scope.remove = function(chat) {
-  //   Chats.remove(chat);
-  // };
 })
 
 .controller('NewsletterBycatCtrl', function($scope, $stateParams, Newsletters) {
-  $scope.newsletters = Newsletters.bycat($stateParams.category);
+  // $scope.newsletters = Newsletters.bycat($stateParams.category);
+  var newsParams = {'paged':'1', 'orderby': 'date', 'action': 'post_lists', 'posttype': 'post', 'postperpage': '30', 'from': 'main', 'category':$stateParams.category};
+  Newsletters.all().async(newsParams).then(function(d) {
+    $scope.newsletters = d;
+  });
 })
 
 .controller('NewsletterCtrl', function($scope, $stateParams, Newsletters) {
@@ -31,6 +26,7 @@ angular.module('starter.controllers', [])
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
-    enableNotify: true
+    enableNotify: true,
+    enableCountry: true
   };
 });
--- SamsungNews/www/js/services.js
+++ SamsungNews/www/js/services.js
@@ -4,6 +4,16 @@ angular.module('starter.services', [])
   var categories = [
     {'slug':'press-release', 'name':'Press Release','subscribe':true},
     {'slug':'technology', 'name':'Technology', 'subscribe':true},
+    {'slug':'mobile', 'name':'Mobile', 'subscribe':true},
+    {'slug':'view', 'name':'Views', 'subscribe':true},
+    {'slug':'b2b', 'name':'B2B', 'subscribe':true},
+    {'slug':'statements', 'name':'Statements', 'subscribe':true},
+    {'slug':'camera-computing', 'name':'Camera & Computing', 'subscribe':true},
+    {'slug':'home-appliances', 'name':'Home Appliances', 'subscribe':true},
+    {'slug':'design', 'name':'Design', 'subscribe':true},
+    {'slug':'infographics', 'name':'Infographics', 'subscribe':true},
+    {'slug':'citizenship', 'name':'Citizenship', 'subscribe':true},
+    {'slug':'semiconductors', 'name':'Semiconductors', 'subscribe':true},
     {'slug':'tv-audio', 'name':'TV & Audio', 'subscribe':true},
     {'slug':'corporateothers', 'name':'Corporate Others', 'subscribe':true}
   ];
@@ -38,14 +48,11 @@ angular.module('starter.services', [])
   };
 })
 .factory('Newsletters', function($http) {
-  var newsParams = {'paged':'1', 'orderby': 'date', 'action': 'post_lists', 'posttype': 'post', 'postperpage': '10', 'from': 'main'};
   var news = [];
   
-  
   var myService = {
-    async: function() {
-      
-        // $http returns a promise, which has a then function, which also returns a promise
+    async: function(newsParams) {
+      // http://news.samsung.com/global/wp-admin/admin-ajax.php
         var promise = $http({
           method:"POST",
           url:"http://news.samsung.com/global/wp-admin/admin-ajax.php",
@@ -59,6 +66,7 @@ angular.module('starter.services', [])
           data: newsParams    
         }).then(function (response) {
           // The return value gets picked up by the then in the controller.
+          console.log(response);
           news = response.data.content.list;
           return response.data.content.list;
         });
@@ -70,7 +78,7 @@ angular.module('starter.services', [])
   
   return {
     all: function() {
-      return myService;
+      return  myService;
     },
     get: function(newsletterId) {
       console.log(news+" "+newsletterId);
@@ -82,13 +90,7 @@ angular.module('starter.services', [])
       return null;
     },
     bycat: function(category){
-      var bycat = [];
-      for (var i = 0; i < news.length; i++) {
-        if (news[i].category_info.slug === category) {
-          bycat.push(news[i]);
-        }
-      }
-      return bycat;
+       return  myService;
     }
   };
 });
